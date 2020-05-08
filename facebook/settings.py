c0f4701557c0632cb0e7d5cefb6e02b614145102
Your Name <you@example.com>
2020-05-08 21:08:04 +0200

    make me better

--- facebook/settings.py
+++ facebook/settings.py
@@ -110,8 +110,7 @@ DATABASES['default'].update(db_from_env)
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT =  os.path.join(BASE_DIR,'static') 
+
 
 
